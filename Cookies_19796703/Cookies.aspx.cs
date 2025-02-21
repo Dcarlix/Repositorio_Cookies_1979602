32da1a4569074ddae480a0bde6d6144db45bcc96
= <carlitosxd2006@gmail.com>
2025-02-20 21:22:41 -0600

    El primer commit del trabajo de cookies, donde se agrega la pagina principal y la secundaria, pero falta un poco de la funcionalidad

---
+++ Cookies_19796703/Cookies.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cookies_19796703
+{
+    public partial class Cookies : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            Response.Cookies["ddlCategory"].Value = ddlCategory.SelectedValue;
+            Response.Cookies["ddlSupplier"].Value = ddlSupplier.SelectedValue;
+            Response.Cookies["strProduct"].Value = txtProduct.Text;
+            Response.Cookies["strDescription"].Value = txtProduct.Text;
+            Response.Cookies["strImage"].Value = txtImage.Text;
+            Response.Cookies["decPrice"].Value = txtPrice.Text;
+            Response.Cookies["bytNumberInStock"].Value = txtNumberInStock.Text;
+            Response.Cookies["bytNumberOnOrder"].Value = txtNumberOnOrder.Text;
+            Response.Cookies["bytReorderLevel"].Value = txtReorderLevel.Text;
+
+            Response.Redirect("ProductConfirmCookies.aspx");
+
+        }
+    }
+}
---
+++ Cookies_19796703/ProductConfirmCookies.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cookies_19796703
+{
+    public partial class ProductConfirmCookies : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ddlCategory.SelectedValue = Request.Cookies["ddlCategory"].Value;
+            ddlSupplier.SelectedValue = Request.Cookies["ddlSupplier"].Value;
+            lblProduct.Text = Request.Cookies["strProduct"].Value;
+        }
+    }
+}
